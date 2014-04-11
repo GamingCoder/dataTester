ea037b02ba73900df86c3168d457977a73559c47
GamingCoder <coreoder@gmail.com>
2014-04-11 17:46:24 +0200

    Added tests for adding, getting and deleting tests to tests.js test

--- test/tests.js
+++ test/tests.js
@@ -1,15 +1,36 @@
 var test = require('tape');
 var dataTester = require('../index.js');
 
+// tests
+var stringTest = function(data) {
+	return typeof data == 'string';
+};
+
 test('tests object', function(t) {
-	t.plan(3);
+	t.plan(2);
 	var tester = new dataTester();
 	
 	t.equal(typeof tester.tests, 'object', 'should be object');
 	t.deepEqual(tester.tests, {}, 'should be empty');
+});
 
-	tester.tests['string'] = function(data) {
-		return typeof data == 'string';
-	};
-	t.ok(tester.tests.hasOwnProperty('string'), 'string test is set');
+test('add and get test', function(t) {
+	t.plan(1);
+	var tester = new dataTester();
+
+	// Add string test
+	tester.setTest('string', stringTest);
+	t.equal(tester.getTest('string'), tester.tests['string'], 'getTest returns same result as direct access');
+
+});
+
+test('del test', function(t) {
+	t.plan(1);
+	var tester = new dataTester();
+
+	// Add string test
+	tester.setTest('string', stringTest);
+	tester.delTest('string');
+	t.equal(tester.getTest('string'), undefined, 'getTest returns undefined');
+
 });
